309315393d196c3d34703b195de14513b34e2bcf
Marina Vab <rzq995@alumni.ku.dk>
2017-04-01 13:43:17 +0200

    added autogenes files and scripts

---
+++ autophagy_genes_lists/getAutophagyGenes.R
@@ -0,0 +1,84 @@
+
+setwd("~/Bioinformatics MSc UCPH/0_MasterThesis/autophagy_genes_lists")
+
+#autophagy genes cathegories
+lipid<- as.vector(read.table("lipid.txt", as.is = T, header = FALSE))$V1
+phosphatidyl<- as.vector(read.table("phosphatidylinositol.txt", as.is = T, header = FALSE))$V1
+endo_exosomes<- as.vector(read.table("endo_exosomes.txt", as.is = T, header = FALSE))$V1
+transport<- as.vector(read.table("transport.txt", as.is = T, header = FALSE))$V1
+rabs<- as.vector(read.table("rabs_and_effectors.txt", as.is = T, header = FALSE))$V1
+docking_and_fusion<- as.vector(read.table("docking_and_fusion.txt", as.is = T, header = FALSE))$V1
+mito<- as.vector(read.table("mito.txt", as.is = T, header = FALSE))$V1
+autophagic_core<- as.vector(read.table("autophagic_core.txt", as.is = T, header = FALSE))$V1
+transcription_factors<- as.vector(read.table("transcription_factors.txt", as.is = T, header = FALSE))$V1
+receptors_and_ligands<- as.vector(read.table("receptors_and_ligands.txt", as.is = T, header = FALSE))$V1
+mTOR_induction<- as.vector(read.table("induction_mTOR.txt", as.is = T, header = FALSE))$V1
+lysosome<- as.vector(read.table("lysosome.txt", as.is = T, header = FALSE))$V1
+
+
+autophagy_genes <- list (
+  lipid=lipid,
+  phosphatidyl = phosphatidyl,
+  endo_exosomes = endo_exosomes,
+  transport = transport,
+  rabs = rabs,
+  docking_fusion = docking_and_fusion,
+  mito = mito,
+  autoph_core = autophagic_core,
+  transcr_factors = transcription_factors,
+  receptors_ligands = receptors_and_ligands,
+  mTOR_induction = mTOR_induction,
+  lysosome = lysosome
+)
+
+all_auto_genes <- vector(mode="character", length=0)
+for (i in 1:length(autophagy_genes)){
+  print(paste0(names(autophagy_genes[i]), ":       ",length(unique(autophagy_genes[[i]])),"/",length(autophagy_genes[[i]])," unique in this group" ))
+  all_auto_genes<-append(all_auto_genes,unique(autophagy_genes[[i]]))
+}
+length(all_auto_genes) #1392
+length(unique(all_auto_genes))#1183
+all_unique_auto_genes<-unique(all_auto_genes)
+
+write(unique(all_unique_auto_genes), file = "autopahagy_genes.txt")
+
+
+
+
+gene_functions <- vector(mode="character", length=0)
+
+for (name in names(autophagy_genes)) {
+    print(name)
+    for (i in all_unique_auto_genes){
+      
+      if(i %in% autophagy_genes[[name]]){
+          if (is.na(gene_functions[i])) {
+            gene_functions[i]<- name
+          }else{
+            gene_functions[i]<- "multifunction"
+          }
+      }
+      
+    }
+}  
+
+save(gene_functions, file="autophagy_genes_functions.rda")
+
+
+auto_matrix<-as.matrix(gene_functions)
+auto_matrix$genes <-rownames(auto_matrix)
+rownames(auto_matrix)<-NULL
+dim(auto_matrix)
+head(auto_matrix)
+
+save(auto_matrix, file="autophagy_functions.rda")
+
+
+
+
+
+#####
+
+
+
+
